520aedc6f21017f195a98010ffcdd2b5635cf48e
Naman-9 <namancp7@gmail.com>
2024-12-13 15:01:21 +0530

    added tiptap editor and it's extensions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <p>Hello world.</p>
+      Click <Link href="/documents/12345">here</Link> to go to documents
     </div>
   )
 }
