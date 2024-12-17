36b8440df5a7ef7e6cacede12219ae37781fb285
Naman-9 <namancp7@gmail.com>
2024-12-17 21:11:40 +0530

    update and delete documents

---
+++ src/app/(home)/document-menu.tsx
@@ -0,0 +1,57 @@
+import { Button } from '@/components/ui/button';
+import { ExternalLinkIcon, FilePenIcon, MoreVertical, TrashIcon } from 'lucide-react';
+import { Id } from '../../../convex/_generated/dataModel';
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from '@/components/ui/dropdown-menu';
+import { RemoveDialog } from '@/components/remove-dialog';
+import { RenameDialog } from '@/components/rename-dialog';
+
+interface DocumentMenuProps {
+  documentId: Id<'documents'>;
+  title: string;
+  onNewTab: (id: Id<'documents'>) => void;
+}
+
+export const DocumentMenu = ({ documentId, onNewTab, title }: DocumentMenuProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" className="rounded-full ">
+          <MoreVertical className="size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+
+        <RenameDialog documentId={documentId} initialTitle={title}>
+          <DropdownMenuItem
+            onSelect={(e) => e.preventDefault()}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <FilePenIcon className="size-4 mr-2" />
+            Rename
+          </DropdownMenuItem>
+        </RenameDialog>
+
+        <DropdownMenuItem onClick={() => onNewTab(documentId)}>
+          <ExternalLinkIcon className="size-4 mr-2" />
+          Open in new tab
+        </DropdownMenuItem>
+
+        <RemoveDialog documentId={documentId}>
+          <DropdownMenuItem
+            onSelect={(e) => e.preventDefault()}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <TrashIcon className="size-4 mr-2" />
+            Remove
+          </DropdownMenuItem>
+        </RemoveDialog>
+
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
--- src/app/(home)/documents-table.tsx
+++ src/app/(home)/documents-table.tsx
@@ -1,62 +1,70 @@
-import { PaginationStatus } from "convex/react";
+import { PaginationStatus } from 'convex/react';
 
-import { Doc } from "../../../convex/_generated/dataModel";
-import { 
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow
- } from "@/components/ui/table";
-import { LoaderIcon } from "lucide-react";
-import { DocumentRow } from "./document-row";
+import { Doc } from '../../../convex/_generated/dataModel';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { LoaderIcon } from 'lucide-react';
+import { DocumentRow } from './document-row';
+import { Button } from '@/components/ui/button';
 
 interface DocumentsTableProps {
-    documents: Doc<"documents">[] | undefined;
-    loadMore: (numberItems: number) => void;
-    status: PaginationStatus;
+  documents: Doc<'documents'>[] | undefined;
+  loadMore: (numberItems: number) => void;
+  status: PaginationStatus;
 }
 
 export const DocumentsTable = ({ documents, loadMore, status }: DocumentsTableProps) => {
-    return (
-        <div className="max-w-screen-xl mx-auto px-24 py-6 flex flex-col gap-5">
-            {documents === undefined ? (
-                <div className="flex justify-center items-center">
-                    <LoaderIcon className="animate-spin text-muted-foreground size-5"/>
-                </div>
-            ): (
-                <Table className="">
-                    <TableHeader >
-                        <TableRow className="hover:bg-transparent border-none">
-                            <TableHead>Name</TableHead>
-                            <TableHead>&nbsp;</TableHead>
-                            <TableHead className="hidden md:table-cell">Shared</TableHead>
-                            <TableHead className="hidden md:table-cell">Created at</TableHead>
-                        </TableRow>
-                    </TableHeader>
-
-                    {
-                        documents.length === 0 ? (
-                            <TableBody>
-                        <TableRow className="hover:bg-transparent">
-                            <TableCell colSpan={4} className="h-24 text-center text-muted-foreground">
-                                No Documents found.
-                            </TableCell>
-                        </TableRow>
-                    </TableBody>
-                        ) : (
-                            <TableBody>
-                                {
-                            documents.map((document) => (
-                                <DocumentRow key={document._id} document={document} />
-                            ))}
-                            </TableBody>
-                        )
-                    }
-
-                </Table>
-            )}
+  return (
+    <div className="max-w-screen-xl mx-auto px-24 py-6 flex flex-col gap-5">
+      {documents === undefined ? (
+        <div className="flex justify-center items-center">
+          <LoaderIcon className="animate-spin text-muted-foreground size-5" />
         </div>
-    )
-}
+      ) : (
+        <Table className="">
+          <TableHeader>
+            <TableRow className="hover:bg-transparent border-none">
+              <TableHead>Name</TableHead>
+              <TableHead>&nbsp;</TableHead>
+              <TableHead className="hidden md:table-cell">Shared</TableHead>
+              <TableHead className="hidden md:table-cell">Created at</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          {documents.length === 0 ? (
+            <TableBody>
+              <TableRow className="hover:bg-transparent">
+                <TableCell colSpan={4} className="h-24 text-center text-muted-foreground">
+                  No Documents found.
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          ) : (
+            <TableBody>
+              {documents.map((document) => (
+                <DocumentRow key={document._id} document={document} />
+              ))}
+            </TableBody>
+          )}
+        </Table>
+      )}
+
+      <div className="flex item-center justify-center">
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={() => loadMore(5)}
+          disabled={status !== 'CanLoadMore'}
+        >
+          {status === 'CanLoadMore' ? 'Load More' : 'End of results'}
+        </Button>
+      </div>
+    </div>
+  );
+};
