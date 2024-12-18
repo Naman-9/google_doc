c00e50c370454d0f9506e96dabb57473788d1df4
Naman-9 <namancp7@gmail.com>
2024-12-18 19:48:23 +0530

    collaborative margin ruler

--- src/app/documents/[documentId]/Ruler.tsx
+++ src/app/documents/[documentId]/Ruler.tsx
@@ -1,76 +1,78 @@
+import { useMutation, useStorage } from '@liveblocks/react';
 import React, { useRef, useState } from 'react';
 import { FaCaretDown } from 'react-icons/fa';
 
 const markers = Array.from({ length: 83 }, (_, i) => i);
 
 const Ruler = () => {
+  const leftMargin = useStorage((root) => root.leftMargin)?? 56;
+  const setLeftMargin = useMutation(({ storage }, position: number) => {
+    storage.set('leftMargin', position);
+  }, []);
 
-    const [leftMargin, setLeftMargin] = useState(56)
-    const [rightMargin, setRightMargin] = useState(56)
+  const rightMargin = useStorage((root) => root.rightMargin)?? 56;
+  const setRightMargin = useMutation(({ storage }, position: number) => {
+    storage.set('rightMargin', position);
+  }, []);
 
-    const [isDraggingLeft, setIsDraggingLeft] = useState(false)
-    const [isDraggingRight, setIsDraggingRight] = useState(false)
+  const [isDraggingLeft, setIsDraggingLeft] = useState(false);
+  const [isDraggingRight, setIsDraggingRight] = useState(false);
 
-    const rulerRef = useRef<HTMLDivElement>(null);
+  const rulerRef = useRef<HTMLDivElement>(null);
 
-    const handleLeftMouseDown = () => {
-        setIsDraggingLeft(true)
-    }
-    const handleRightMouseDown = () => {
-        setIsDraggingRight(true)
-    }
+  const handleLeftMouseDown = () => {
+    setIsDraggingLeft(true);
+  };
+  const handleRightMouseDown = () => {
+    setIsDraggingRight(true);
+  };
 
-    const handleMouseMove = (e: React.MouseEvent) => {
+  const handleMouseMove = (e: React.MouseEvent) => {
+    const PAGE_WIDTH = 816;
+    const MINIUM_SPACE = 100;
 
-        const PAGE_WIDTH = 816;
-        const MINIUM_SPACE =  100;
+    if ((isDraggingLeft || isDraggingRight) && rulerRef.current) {
+      const container = rulerRef.current.querySelector('#ruler-container');
 
-        if((isDraggingLeft || isDraggingRight) && rulerRef.current){
-            const container = rulerRef.current.querySelector('#ruler-container');
+      if (container) {
+        const containerRect = container?.getBoundingClientRect();
+        const relativeX = e.clientX - containerRect?.left;
+        const rawPosition = Math.max(0, Math.min(PAGE_WIDTH, relativeX));
 
-            if(container) {
-                const  containerRect = container?.getBoundingClientRect();
-                const relativeX = e.clientX - containerRect?.left;
-                const rawPosition = Math.max(0, Math.min(PAGE_WIDTH, relativeX));
-
-                if(isDraggingLeft){
-                  const maxLeftPosition = PAGE_WIDTH - (rightMargin - MINIUM_SPACE);
-                  const newLeftPosition = Math.min(rawPosition, maxLeftPosition);
-                  setLeftMargin(newLeftPosition);  // TODO: Make it collabrative
-
-                } else if (isDraggingRight) {
-                  const maxRightPosition = PAGE_WIDTH - (leftMargin +  MINIUM_SPACE);
-                  const newRightPosition = Math.max(PAGE_WIDTH - rawPosition, 0);
-                  const constrainedRightPosition = Math.min(newRightPosition, maxRightPosition);
-                  setRightMargin(constrainedRightPosition);
-                }
-            }
+        if (isDraggingLeft) {
+          const maxLeftPosition = PAGE_WIDTH - (rightMargin - MINIUM_SPACE);
+          const newLeftPosition = Math.min(rawPosition, maxLeftPosition);
+          setLeftMargin(newLeftPosition); // TODO: Make it collabrative
+        } else if (isDraggingRight) {
+          const maxRightPosition = PAGE_WIDTH - (leftMargin + MINIUM_SPACE);
+          const newRightPosition = Math.max(PAGE_WIDTH - rawPosition, 0);
+          const constrainedRightPosition = Math.min(newRightPosition, maxRightPosition);
+          setRightMargin(constrainedRightPosition);
         }
-
       }
+    }
+  };
 
-      const handleMouseUp = () => {
-        setIsDraggingLeft(false);
-        setIsDraggingRight(false);
-      }
+  const handleMouseUp = () => {
+    setIsDraggingLeft(false);
+    setIsDraggingRight(false);
+  };
 
-      const handleLeftDoubleClick = () => {
-        setLeftMargin(56);
-      }
+  const handleLeftDoubleClick = () => {
+    setLeftMargin(56);
+  };
 
-      const handleRightDoubleClick = () => {
-        setRightMargin(56);
-      }
-
-
+  const handleRightDoubleClick = () => {
+    setRightMargin(56);
+  };
 
   return (
-    <div 
-        className="w-[816px] mx-auto h-6 border-b border-gray-300 flex items-end relative select-none print:hidden"
-        ref={rulerRef}
-        onMouseMove= {handleMouseMove}
-        onMouseUp={handleMouseUp}    
-        onMouseLeave={handleMouseUp}
+    <div
+      className="w-[816px] mx-auto h-6 border-b border-gray-300 flex items-end relative select-none print:hidden"
+      ref={rulerRef}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
     >
       <div className=" w-full h-full relative" id="ruler-container">
         <Marker
@@ -139,16 +141,16 @@ const Marker = ({ position, isLeft, isDragging, onMouseDown, onDoubleClick }: Ma
       onDoubleClick={onDoubleClick}
     >
       <FaCaretDown className="absolute left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2" />
-      <div 
+      <div
         className="absolute left-1/2 top-4 transform -translate-x-1/2 transition-opacity duration-150"
         style={{
-          height: "100vh",
-          width: "1px",
-          transform:"scaleX(0.5)",
-          backgroundColor: "#3b72f6",
-          display: isDragging ? "block" : "none"
+          height: '100vh',
+          width: '1px',
+          transform: 'scaleX(0.5)',
+          backgroundColor: '#3b72f6',
+          display: isDragging ? 'block' : 'none',
         }}
-        ></div>
+      ></div>
     </div>
   );
 };
--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -68,7 +68,7 @@ export function Room({ children }: { children: ReactNode }) {
         return filteredUsers.map((user) => user.id)
       }}
     >
-      <RoomProvider id={params.documentId as string}>
+      <RoomProvider id={params.documentId as string} initialStorage={{ leftMargin: 56, rightMargin: 56}}>
         <ClientSideSuspense fallback={
           <FullscreenLoader label='Room loading...'/>
         }>{children}</ClientSideSuspense>
